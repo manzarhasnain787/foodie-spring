93d631f630b8bace618601547c43066fab96ecde
nirajgeorgian <nirajgeorgian@gmail.com>
2019-02-18 17:25:39 +0530

    completed spring java project

---
+++ src/main/java/com/dodo/foodie/controllers/OrderController.java
@@ -0,0 +1,61 @@
+package com.dodo.foodie.controllers;
+
+import com.dodo.foodie.models.Order;
+import com.dodo.foodie.models.User;
+import com.dodo.foodie.repository.OrderRepository;
+import com.dodo.foodie.web.OrderProps;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("order")
+public class OrderController {
+
+    private OrderRepository orderRepository;
+    private OrderProps props;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository, OrderProps props) {
+        this.orderRepository = orderRepository;
+        this.props = props;
+    }
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, props.getPageSize());
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus, @AuthenticationPrincipal User user) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+        order.setUser(user);
+        orderRepository.save(order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+}
